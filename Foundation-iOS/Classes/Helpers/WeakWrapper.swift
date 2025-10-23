e966443328253beeaa8feaf21f6a90d05940e7e7
Russel <emkil.russel@gmail.com>
2025-10-23 11:51:52 +0200

    fix wrapper

--- Foundation-iOS/Classes/Helpers/WeakWrapper.swift
+++ Foundation-iOS/Classes/Helpers/WeakWrapper.swift
@@ -13,21 +13,3 @@ public extension [WeakWrapper] {
         self = filter { $0.target != nil }
     }
 }
-
-public final class WeakObserver {
-    public weak var target: AnyObject?
-    public let notificationQueue: DispatchQueue
-    public let closure: () -> Void
-
-    public init(target: AnyObject, notificationQueue: DispatchQueue, closure: @escaping () -> Void) {
-        self.target = target
-        self.notificationQueue = notificationQueue
-        self.closure = closure
-    }
-}
-
-public extension [WeakObserver] {
-    mutating func clearEmptyItems() {
-        self = filter { $0.target != nil }
-    }
-}
